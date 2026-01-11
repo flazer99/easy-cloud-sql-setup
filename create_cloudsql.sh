5566e65f7a0c34b83e7f3928d8c1ec5d0d905720
Abirami Sukumaran <abidsukumaran@gmail.com>
2026-01-11 21:40:09 +0530

    Enable private IP Google access for subnet creation
    Added option to enable private IP Google access when creating a subnet.

--- create_cloudsql.sh
+++ create_cloudsql.sh
@@ -51,7 +51,8 @@ if ! gcloud compute networks subnets describe $SUBNET_NAME --region=$REGION > /d
     gcloud compute networks subnets create $SUBNET_NAME \
         --region=$REGION \
         --network=$VPC_NAME \
-        --range="10.0.0.0/24"
+        --range="10.0.0.0/24" \
+        --enable-private-ip-google-access
 else
     echo "Subnet $SUBNET_NAME already exists. Skipping."
 fi
